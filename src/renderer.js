aa6646e969e75d5b633e2b68e3eb2a06f892a30b
r1sque <minatto24@gmail.com>
2025-02-01 21:14:28 +0100

    js

--- src/renderer.js
+++ src/renderer.js
@@ -18,3 +18,46 @@ document
 window.electronAPI.onPageContent((content) => {
   document.getElementById("mainContent").innerHTML = content;
 });
+
+
+// Use the exposed `electronAPI` to send the "launch-minecraft" event
+document.getElementById("launch-button").addEventListener("click", () => {
+  window.electronAPI.send("launch-minecraft");
+});
+
+function filterList() {
+  const input = document.getElementById("search").value.toLowerCase();
+  const items = document.querySelectorAll(".version-item");
+
+  items.forEach(item => {
+    const version = item.getAttribute("data-version");
+    if (version.includes(input)) {
+      item.style.display = "";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Use event delegation to attach event listener to a parent element
+  const mainContent = document.getElementById("mainContent");
+
+  // Check if the button is clicked inside the mainContent
+  mainContent.addEventListener("click", function (event) {
+    if (event.target && event.target.id === "addVersionButton") {
+      const overlay = document.getElementById("overlay");
+      if (overlay) {
+        overlay.classList.remove("hidden");
+      }
+    }
+
+    // Handle closing the overlay
+    if (event.target && event.target.id === "closeOverlay") {
+      const overlay = document.getElementById("overlay");
+      if (overlay) {
+        overlay.classList.add("hidden");
+      }
+    }
+  });
+});
